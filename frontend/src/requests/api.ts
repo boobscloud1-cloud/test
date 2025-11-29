7b6ec5693087764e9e69257a57b4360733a15bc3
unknown <ralf.nagelmann@torg.ee>
2025-11-29 19:02:11 +0200

    Update environment URLs

--- frontend/src/requests/api.ts
+++ frontend/src/requests/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000'; // Change for production
+const API_URL = import.meta.env.VITE_API_URL || 'https://test-xvll.onrender.com';
 
 export const api = axios.create({
   baseURL: API_URL,
